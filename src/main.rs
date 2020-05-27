733a43380f8580bcecdb7235f35dd2ff040ec74a
alexmatthew Candelario <alexmatthewcandelario@candtech.home>
2020-05-27 18:04:19 -0400

    added ablility close task

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,61 @@ impl TodoItem {
     }
 }
 
+struct TodoList {
+    list: Vec<TodoItem>,
+}
+
+impl TodoList {
+    fn new() -> TodoList {
+        return TodoList { list: Vec::new() };
+    }
+
+    fn add_todo_list(&mut self, name: String) {
+        let todo_item = TodoItem::new(name);
+        self.list.push(todo_item);
+    }
+
+    fn mark_as_done(&mut self, index: usize) {
+        self.list[index].completed = 'x'
+    }
+
+    fn print(&self) {
+        for (index, item) in self.list.iter().enumerate() {
+            println!("{} [{}] - {}", index, item.completed, item.name);
+        }
+    }
+}
+
+enum Command {
+    Get,
+    Add(String),
+    Done(usize),
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let command = args[1].clone();
 
-    let todo_item = TodoItem::new("Do someting in Rust".to_string());
+    let command = match args[1].as_str() {
+        "get" => Command::Get,
+        "add" => Command::Add(args[2].clone()),
+        "done" => Command::Done(args[2].parse().expect("Error in entering the right type")),
+        _ => panic!("You must provide an accepted command"),
+    };
 
-    let todo_list = vec![todo_item];
+    let mut todo_list = TodoList::new();
 
-    if command == "get" {
-        for item in todo_list {
-            println!("[{}] - {}", item.completed, item.name);
+    todo_list.add_todo_list("Do Something With Rust".to_string());
+    todo_list.add_todo_list("Create a CLI with Rust".to_string());
+
+    match command {
+        Command::Get => todo_list.print(),
+        Command::Add(task) => {
+            todo_list.add_todo_list(task);
+            todo_list.print();
+        }
+        Command::Done(index) => {
+            todo_list.mark_as_done(index);
+            todo_list.print();
         }
     }
 }
